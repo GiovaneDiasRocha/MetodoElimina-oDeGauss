f441926a4c5d1ffd3a2a4ba8d6f4e15f8d82c074
GiovaneDiasRocha <66972321+GiovaneDiasRocha@users.noreply.github.com>
2022-05-10 18:26:47 -0300

    Atualização código

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class MetodoEliminaçãoDeGauss {
+    public static int [,] matriz () {
+        int [,] matrizTeste = {{2,1,4},{2,1,3}};
+        return matrizTeste;
+    }
+
+    public static int multiplicador(int l) {
+        int pivo = 0;
+            int multiplicador = 0;
+        for (int i=0; i <= matriz().GetLength(0); i++) {
+                for (int j=0; j <= matriz().GetLength(0); j++) {
+                    pivo = matriz()[l,l];
+                    return multiplicador = matriz()[i,j]/pivo;
+                }
+            }
+        return multiplicador;
+    }
+
+    public static void atualizarMatriz(int [,] matriz, int valorAtualiazado, int posI, int posJ) {
+
+        
+    }
+
+    public static void eliminacaoGauss () {
+        int pivo = 0;
+        int valorLi = 0;
+        for (int i=1; i <= matriz().GetLength(0); i++) {
+                for (int j=1; j <= matriz().GetLength(1); j++) {
+                    if (i == 1) {
+                        break;
+                    }
+                    pivo = matriz()[i,i];
+                    valorLi = multiplicador(i - 1);
+                }
+            }
+    }
+
+    public static void Main () {
+        Console.WriteLine(eliminacaoGauss());
+    }
+}
