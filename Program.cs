9158547701281216cde613d92c16ccba5e78f24b
GiovaneDiasRocha <66972321+GiovaneDiasRocha@users.noreply.github.com>
2022-05-17 22:04:50 -0300

    Atualização

--- Program.cs
+++ Program.cs
@@ -1,66 +1,70 @@
 ﻿public class MetodoEliminaçãoDeGauss
 {
-    public static float[,] matriz()
+    public static float multiplicador(float [, ] matriz, int l, int etapa)
     {
-        float[,] matrizTeste = { { 3, 2, 4, 1 }, { 1, 1, 2, 2 }, { 4, 3, -2, 3 } };
-        return matrizTeste;
-    }
-
-    public static float multiplicador(int l, int etapa)
-    {
-        float pivo = matriz()[etapa - 1, etapa - 1];
-        float mult = matriz()[l, etapa - 1] / pivo;
+        float pivo = matriz[etapa - 1, etapa - 1];
+        float mult = matriz[l, etapa - 1] / pivo;
         return mult;
     }
 
-    public static float [,] eliminacaoGauss(float[,] matriz, int quantLinhasMatriz, int quantColunasMatriz, int etapa = 1)
-    {
+    public static float [,] eliminacaoGauss(float[,] matriz, int quantLinhasMatriz, int quantColunasMatriz, int etapa)
+    {   
         float[,] novaMatriz = new float[quantLinhasMatriz, quantColunasMatriz];
         for (int i = 0; i < quantLinhasMatriz; i++)
         {
             float mult = 0;
             if (i != 0 && i < quantLinhasMatriz)
             {
-                mult = multiplicador(i, etapa);
+                mult = multiplicador(matriz, i, etapa);
             }
             for (int j = 0; j < quantColunasMatriz; j++)
             {
                 novaMatriz[i, j] = matriz[i, j];
                 if (i >= etapa)
-                {
-                    novaMatriz[i, j] = matriz[i, j] - mult * matriz[etapa - 1, j];
+                {   
+                    matriz[i, j] = matriz[i, j] - mult * matriz[etapa - 1, j];
                 }
             }
         }
-        etapa++;//Ao invés de fazer método recursivo, tentar fazer a etapa ser passada na chamada do método
-        if (etapa < quantLinhasMatriz)
-        {   
-            
-            eliminacaoGauss(novaMatriz, quantLinhasMatriz, quantColunasMatriz, etapa);
-        } else {
-            return novaMatriz;
-        }
-        return novaMatriz;
+        return matriz;
     }
 
     public static void Main()
     {
 
-        float[,] matriz = { { 3, 2, 4, 1 }, { 1, 1, 2, 2 }, { 4, 3, -2, 3 } };
+        float[,] matriz = { { 6, 2, -1, 7 }, { 2, 4, 1, 7 }, { 3, 2, 8, 13 } };
         int quantLinhasMatriz = matriz.GetLength(0);
         int quantColunasMatriz = matriz.GetLength(1);
         float[,] novaMatriz = new float[quantLinhasMatriz, quantColunasMatriz];
-        novaMatriz = eliminacaoGauss(matriz, quantLinhasMatriz, quantColunasMatriz);
-        
 
-        for (int i = 0; i < matriz.GetLength(0); i++)
+        for (int i = 1; i < quantLinhasMatriz; i++)
         {
-            for (int j = 0; j < matriz.GetLength(1); j++)
+            matriz = eliminacaoGauss(matriz, quantLinhasMatriz, quantColunasMatriz, i);
+        }
+
+        //Calcular x1, x2, x3
+
+        //matriz[0, 0]x1      + matriz[0, 1]x2 + matriz[0, 2]x3 = matriz[0, 3]
+        //                      matriz[1, 1]x2 + matriz[1, 2]x3 = matriz[1, 3]
+        //                                       matriz[2, 2]x3 = matriz[2, 3]
+
+        float x1, x2, x3;
+
+        x3 = matriz[2, 3] / matriz[2, 2];
+        x2 = (matriz[1, 3] - matriz[1, 2] * x3) / matriz[1, 1];
+        x1 = (matriz[0, 3] - matriz[0, 1] * x2 - matriz[0, 2] * x3) / matriz[0, 0];
+
+        for (int i = 0; i < quantLinhasMatriz; i++)
+        {
+            for (int j = 0; j < quantColunasMatriz; j++)
             {
-                Console.Write(novaMatriz[i, j] + " ");
+                Console.Write(matriz[i, j] + " ");
             }
             Console.Write("\n");
         }
 
+        Console.WriteLine(x1);
+        Console.WriteLine(x2);
+        Console.WriteLine(x3);
     }
 }
