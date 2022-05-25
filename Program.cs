7eead567673a9b5e35a97aba93bc6775ffbc9da7
GiovaneDiasRocha <66972321+GiovaneDiasRocha@users.noreply.github.com>
2022-05-24 22:10:38 -0300

    Atualização Código
    Lembrar de inverter a ordem que o valor de x é calculado.

--- Program.cs
+++ Program.cs
@@ -29,17 +29,43 @@
         return matriz;
     }
 
+    public static float [] somatorio (float [,] matriz, int quantLinhasMatriz, int quantColunasMatriz) {
+
+        float [] resultadoSomatorio = new float [quantLinhasMatriz];
+        float resultadoAux = 0;
+        int n = 0, j = 0;
+        for (int i = 0; i < quantLinhasMatriz; i++)
+        {
+            while (n < quantLinhasMatriz) {
+                j++;
+                resultadoAux += matriz[i, j] * resultadoSomatorio[n];
+                n++;
+            }
+            resultadoSomatorio[i] = resultadoAux / matriz[i, i];
+        }
+
+        return resultadoSomatorio;
+    }
+
     public static void Main()
     {
 
         float[,] matriz = { { 6, 2, -1, 7 }, { 2, 4, 1, 7 }, { 3, 2, 8, 13 } };
         int quantLinhasMatriz = matriz.GetLength(0);
         int quantColunasMatriz = matriz.GetLength(1);
-        float[,] novaMatriz = new float[quantLinhasMatriz, quantColunasMatriz];
+        //float[,] novaMatriz = new float[quantLinhasMatriz, quantColunasMatriz];
 
         for (int i = 1; i < quantLinhasMatriz; i++)
         {
             matriz = eliminacaoGauss(matriz, quantLinhasMatriz, quantColunasMatriz, i);
+        }
+
+        float [] resultado = new float [quantLinhasMatriz];
+        resultado = somatorio(matriz, quantLinhasMatriz, quantColunasMatriz);
+
+        foreach (float x in resultado)
+        {
+            Console.WriteLine(x);
         }
 
         //Calcular x1, x2, x3
@@ -48,11 +74,11 @@
         //                      matriz[1, 1]x2 + matriz[1, 2]x3 = matriz[1, 3]
         //                                       matriz[2, 2]x3 = matriz[2, 3]
 
-        float x1, x2, x3;
+        //float x1, x2, x3;
 
-        x3 = matriz[2, 3] / matriz[2, 2];
-        x2 = (matriz[1, 3] - matriz[1, 2] * x3) / matriz[1, 1];
-        x1 = (matriz[0, 3] - matriz[0, 1] * x2 - matriz[0, 2] * x3) / matriz[0, 0];
+        // x3 = matriz[2, 3] / matriz[2, 2];
+        // x2 = (matriz[1, 3] - matriz[1, 2] * x3) / matriz[1, 1];
+        // x1 = (matriz[0, 3] - matriz[0, 1] * x2 - matriz[0, 2] * x3) / matriz[0, 0];
 
         for (int i = 0; i < quantLinhasMatriz; i++)
         {
@@ -62,9 +88,9 @@
             }
             Console.Write("\n");
         }
-
-        Console.WriteLine(x1);
-        Console.WriteLine(x2);
-        Console.WriteLine(x3);
+        
+        // Console.WriteLine(x1);
+        // Console.WriteLine(x2);
+        // Console.WriteLine(x3);
     }
 }
